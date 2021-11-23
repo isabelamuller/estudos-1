42e025b02320cc35518b20d65d403b4c98e25557
Isabela Müller <94134546+isabelamuller@users.noreply.github.com>
2021-11-23 19:00:05 -0300

    Add files via upload
    primeiro mini estudo de uma simples array E FINALMENTE ACHO QUE APRENDI A POSTAR NO GITHUB!!!!! mas pode ser que to fazendo tudo errado tb.. to indo pela intuiçao.. serio que app complicado!!!!

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] arr = {"teste1", "teste2", "nao sei", "to c fome", "adeus", "hello"};
+
+        String x = arr[5];
+        System.out.println(x);
+    }
+}
+    // isa basicamente aqui tu criou uma array de 5 palavras e dai ali no arr[] tu escolhe um numero de
+    // 0 a 4, dai vai aparecer a palavra q ta nessa posiçao UHUUUUUUU dai se tu quer add mais palavras so
+    // muda o numero la de cima dentro do [] em new String SACAS??? :)
